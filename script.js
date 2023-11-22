bbdbb430d3c37fa69afd8252babc306cf31cd2b9
devmuhib009 <142894870+devmuhib009@users.noreply.github.com>
2023-11-22 11:19:00 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
