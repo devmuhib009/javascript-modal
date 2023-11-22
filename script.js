f7170d3e3c3ea1679e8445415121ea93fda1d6a9
devmuhib009 <142894870+devmuhib009@users.noreply.github.com>
2023-11-22 12:30:44 +0600

    Modal Practice

--- script.js
+++ script.js
@@ -5,13 +5,6 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-let i = 0;
-
-while (i < btnsOpenModal.length) {
-  console.log(btnsOpenModal[i].textContent);
-  i++;
-}
-
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -21,3 +14,21 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  });
+
+  // window.addEventListener('keydown', function (e) {
+  //   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+  //     closeModal();
+  //   }
+  // });
+}
